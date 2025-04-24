571de1ea0ac4a3cd52dbbcf0a69b943e8a5e0a58
Omar Mojaddidi <omar.m.mojaddidi@gmail.com>
2025-04-23 21:34:56 -0400

    Adding item spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,11 +1,37 @@
+using Meta.WitAi;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject myItem;
+    public GameObject cheese;
+    public GameObject burger;
+    public GameObject bunTop;
+    public GameObject bunBot;
+    public GameObject lettuce;
+    public GameObject tomato;
 
-    public void SpawnSphere()
+    public void SpawnCheese()
     {
-        Instantiate(myItem);
+        Instantiate(cheese);
+    }
+
+    public void SpawnBurger()
+    {
+        Instantiate(burger);
+    }
+    public void SpawnLettuce()
+    {
+        Instantiate(lettuce);
+    }
+    
+    public void SpawnBun()
+    {
+        Instantiate(bunTop);
+        Instantiate(bunBot);
+    }
+    
+    public void SpawnTomato()
+    {
+        Instantiate(tomato);
     }
 }
