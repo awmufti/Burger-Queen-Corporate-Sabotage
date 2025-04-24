1b9343db1ebc0ac3a19d15424cc52195039d82c7
Abdul Wahab Mufti <awmufti3@gmail.com>
2025-04-24 02:59:10 -0400

    finished npc movement
    - they turn around too
    - if u press space when they are at the counter, they move
    - the space key can be changed to watever

--- Assets/Scripts/NPC_Talking.cs
+++ Assets/Scripts/NPC_Talking.cs
@@ -5,27 +5,50 @@ public class NPC_Talking : MonoBehaviour
 {
     public GameObject NPC_talking;
     public GameObject otherNPC_talking;
-    
-    private bool npcCanStartWalking = false;
+
+    private bool npcCanStartWalking = true;
     private bool otherNpcCanStartWalking = false;
 
     private Vector3 npc_pos;
     private Vector3 othernpc_pos;
+
+    private bool npcGoingToCounter = true;
+    private bool otherNpcGoingToCounter = true;
+
+    private bool npcPausedAtCounter = false;
+    private bool otherNpcPausedAtCounter = false;
+
 
     void Start()
     {
         npc_pos = NPC_talking.transform.position;
         othernpc_pos = otherNPC_talking.transform.position;
 
+        NPC_talking.SetActive(true);         // NPC1 starts at door
+        otherNPC_talking.SetActive(false);   // NPC2 hidden at start
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            npcCanStartWalking = true;
+            if (npcPausedAtCounter)
+            {
+                npcPausedAtCounter = false;
+                npcCanStartWalking = true;
+                npcGoingToCounter = false; // walk back
+                NPC_talking.transform.rotation = Quaternion.Euler(89.98f, 0, 0);
+            }
+            else if (otherNpcPausedAtCounter)
+            {
+                otherNpcPausedAtCounter = false;
+                otherNpcCanStartWalking = true;
+                otherNpcGoingToCounter = false; // walk back
+                otherNPC_talking.transform.rotation = Quaternion.Euler(89.98f, 0, 0);
+            }
         }
+
+
         NPC_Walk();
         NPC_Stop();
         otherNPC_Walk();
@@ -34,42 +57,58 @@ public class NPC_Talking : MonoBehaviour
 
     void NPC_Walk()
     {
-        if(npcCanStartWalking && !otherNPC_talking.activeSelf)
+        if (npcCanStartWalking)
         {
-            npc_pos.z -= 1 * Time.deltaTime;
-            transform.position = npc_pos;
-            UnityEngine.Debug.Log("walk");
+            float direction = npcGoingToCounter ? 2 : -2;
+            npc_pos.z += direction * Time.deltaTime;
+            NPC_talking.transform.position = npc_pos;
         }
     }
 
     void NPC_Stop()
     {
-        if (npcCanStartWalking && npc_pos.z <-48)
+        if (npcGoingToCounter && npc_pos.z >= -33f)
         {
             npcCanStartWalking = false;
+            npcPausedAtCounter = true;
+        }
+        else if (!npcGoingToCounter && npc_pos.z <= -49f)
+        {
+            npcCanStartWalking = false;
+            NPC_talking.SetActive(false);        // hide NPC1
+            npcGoingToCounter = true;            // reset for next cycle
             otherNpcCanStartWalking = true;
-            NPC_talking.SetActive(false);
-            otherNPC_talking.SetActive(true);
+            otherNPC_talking.SetActive(true);    // show NPC2
+            NPC_talking.transform.rotation = Quaternion.Euler(89.98f, 0, -180);
         }
     }
 
     void otherNPC_Walk()
     {
-        if (otherNpcCanStartWalking && !NPC_talking.activeSelf)
+        if (otherNpcCanStartWalking)
         {
-            othernpc_pos.z += 1 * Time.deltaTime;
+            float direction = otherNpcGoingToCounter ? 2 : -2;
+            othernpc_pos.z += direction * Time.deltaTime;
             otherNPC_talking.transform.position = othernpc_pos;
         }
     }
 
     void otherNPC_Stop()
     {
-        if (othernpc_pos.z > -33)
+        if (otherNpcGoingToCounter && othernpc_pos.z >= -33f)
         {
             otherNpcCanStartWalking = false;
+            otherNpcPausedAtCounter = true;
+        }
+        else if (!otherNpcGoingToCounter && othernpc_pos.z <= -49f)
+        {
+            otherNpcCanStartWalking = false;
+            otherNPC_talking.SetActive(false);     // hide NPC2
+            otherNpcGoingToCounter = true;         // reset for next time
+            NPC_talking.SetActive(true);
             npcCanStartWalking = true;
-            NPC_talking.SetActive(false);
-            otherNPC_talking.SetActive(true);
+            otherNPC_talking.transform.rotation = Quaternion.Euler(89.98f, 0, -180);
         }
     }
+
 }
